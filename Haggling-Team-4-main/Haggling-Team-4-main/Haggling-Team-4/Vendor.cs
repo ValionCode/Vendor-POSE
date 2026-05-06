3dc9cbaa0885027d2cfb60181510a585e909792b
FabiaCode <f.baumgartner@students.htl-leondin.ac.at>
2026-05-06 16:45:13 +0200

    Properties and Constructor

---
+++ Haggling-Team-4-main/Haggling-Team-4-main/Haggling-Team-4/Vendor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haggling_Team_4;
+public class Vendor
+{
+    public Dictionary<string, Product> InventoryPrices { get; private set; }
+    public double Money { get; private set; }
+    public int Patience { get; private set; }
+
+    public Vendor(double initialMoney)
+    {
+        Money = initialMoney;
+        Patience = 10;
+        InventoryPrices = new Dictionary<string, Product>();
+    }
+}
--- Haggling-Team-4-main/Haggling-Team-4-main/Haggling-Team-4/obj/Debug/net9.0/Haggling-Team-4.AssemblyInfo.cs
+++ Haggling-Team-4-main/Haggling-Team-4-main/Haggling-Team-4/obj/Debug/net9.0/Haggling-Team-4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haggling-Team-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74ee5af9b39da82bcb1c34521c4e88e94d6c879")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haggling-Team-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haggling-Team-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
